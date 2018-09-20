2e63dfc30df706366e4e6c09ebf2ad1c3f0c1bea
Fancy <553973204@qq.com>
2018-09-20 11:27:29 +0800

    调整分录接口

--- administrative-service/src/main/java/com/avatech/edi/administrative/service/VoucherService.java
+++ administrative-service/src/main/java/com/avatech/edi/administrative/service/VoucherService.java
@@ -1,5 +1,8 @@
 package com.avatech.edi.administrative.service;
 
+import com.avatech.edi.administrative.businessone.journal.B1JournalEntryService;
+import com.avatech.edi.administrative.config.B1Manager;
+import com.avatech.edi.administrative.model.bo.B1Connection;
 import com.avatech.edi.administrative.model.bo.IVoucher;
 import com.avatech.edi.administrative.model.bo.Voucher;
 import com.avatech.edi.administrative.repository.IVoucherRepository;
@@ -12,10 +15,16 @@ public class VoucherService {
     @Autowired
     private IVoucherRepository voucherRepository;
 
+    @Autowired
+    private B1Manager b1Manager;
+
+    @Autowired
+    private B1JournalEntryService b1JournalEntryService;
+
+
+
     public void saveVoucher(Voucher voucher){
-
-        voucher.setDocEntry(1);
-
-        voucherRepository.save(voucher);
+        B1Connection connection = b1Manager.getB1ConnInstance(voucher.getCompanyName());
+        b1JournalEntryService.createJournalEntry(voucher,connection);
     }
 }
