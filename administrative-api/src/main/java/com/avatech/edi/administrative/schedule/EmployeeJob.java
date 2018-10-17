23fce8ff87880acb212a359861d09530a612f935
okzhangyu <41094697+okzhangyu@users.noreply.github.com>
2018-10-17 20:14:02 +0800

    接口的修改
    配置公司信息

--- administrative-api/src/main/java/com/avatech/edi/administrative/schedule/EmployeeJob.java
+++ administrative-api/src/main/java/com/avatech/edi/administrative/schedule/EmployeeJob.java
@@ -2,6 +2,7 @@ package com.avatech.edi.administrative.schedule;
 
 import com.avatech.edi.administrative.config.HttpRequest;
 import com.avatech.edi.administrative.data.OpType;
+import com.avatech.edi.administrative.model.bo.Department;
 import com.avatech.edi.administrative.model.bo.Employee;
 import com.avatech.edi.administrative.model.bo.TaskRecord;
 import com.avatech.edi.administrative.model.config.MasterDataType;
@@ -11,7 +12,6 @@ import com.avatech.edi.administrative.service.EmployeeService;
 import com.avatech.edi.administrative.service.TaskService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.management.jmx.TraceNotification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,104 +47,66 @@ public class EmployeeJob {
     private void process(){
         processData(OpType.ADD);
         processData(OpType.UPDATE);
-        processData(OpType.UPDATE);
-
+        processData(OpType.DELETE);
+       
     }
 
     private void processData(String opType) {
         try {
-                List<TaskRecord> taskRecords = taskService.fetchTaskList(MasterDataType.EMPLOYEE, opType);
+            List<TaskRecord> taskRecords = taskService.fetchTaskList(MasterDataType.EMPLOYEE, opType);
             if (taskRecords.size() == 0)
                 return;
             logger.info(">>>>>>>>>>>>>>获取未同步员工主数据{" + taskRecords.size() + "}条");
-//            List<Employee> employees = employeeService.fetchEmployees(taskRecords);
-//            logger.info(">>>>>>>>>>>>>>同步员工主数据:" + employees.toString());
+            List<Employee> employees = employeeService.fetchEmployees(taskRecords);
+            logger.info(">>>>>>>>>>>>>>同步员工主数据:" + employees.toString());
             RestTemplate template = new RestTemplate();
-            Employee employee;
-            ResponseEntity<String> result= null;
-            for(TaskRecord record:taskRecords){
-                employee=employeeService.fetchEmployee(record);
-                if(record==null){
-                    record.setIsSync("E");
-                    record.setSyncMessage("未找到员工信息");
-                    taskService.updateTask(record);
-                    continue;
-                }
-                if (OpType.ADD.equals(opType)){
+            ResponseEntity<String> result=null;
+            for (Employee employee:employees) {
+                try{
+                    // get department id by department code
                     ResponseEntity<String> resDept = template.getForEntity(request.getOrgUrl(MasterDataType.DEPARTMENT, employee.getDepartCode()), String.class);
-                }
-                else {
-                    HttpHeaders headers = new HttpHeaders();
-                    MimeType mimeType = MimeTypeUtils.parseMimeType("application/json");
-                    MediaType mediaType = new MediaType(mimeType.getType(), mimeType.getSubtype(), Charset.forName("UTF-8"));
-                    headers.setContentType(mediaType);
-                    if (OpType.UPDATE.equals(opType)) {
-                        HttpEntity<Map> entity = new HttpEntity<Map>(Employee.createEmpMap(employee), headers);
-                        result = template.exchange(request.getRequestUrl(MasterDataType.DEPARTMENT, employee.getDepartCode()), HttpMethod.PUT, entity, String.class);
+                    if(resDept.hasBody()){
+                        List<DefaultValue> defaultValues = mapper.readValue(result.getBody(), new TypeReference<List<DefaultValue>>() {});
+                        if(defaultValues.size() == 0) {
+                            logger.error(">>>>>>>>>>>>>>同步员工主数据失败：根据员工部门code在OA未找到匹配的部门");
+                            return;
+                        }
+                        employee.setOrgDepartmentId(defaultValues.get(0).getId());
+                    }
+                    // call oa service push employee
 
-
-                }
-                else if(OpType.DELETE.equals(opType)){
-                    result=template.exchange(request.getDeleteUrl(MasterDataType.DEPARTMENT,employee.getDepartCode()),HttpMethod.PUT,null,String.class);
-                }
-
-                }
-                if (result.hasBody()){
-                    OrgResponse res = (OrgResponse) mapper.readValue(result.getBody(), OrgResponse.class);
-                    if(res!=null){
-                        record.setIsSync("E");
-                        record.setSyncMessage("同步结果失败返回结果为空");
-
+                    Map emMap = Employee.createEmpMap(employee);
+                    String jsonResult = mapper.writerWithDefaultPrettyPrinter()
+                            .writeValueAsString(emMap);
+                    if(OpType.ADD.equals(opType)) {
+                        result = template.postForEntity(request.getRequestUrl(MasterDataType.EMPLOYEE, opType), emMap, String.class);
                     }
                     else {
-                        if (res.getSuccess().equals("Successful")){
-                        record.setIsSync("Y");
-                        record.setSyncMessage("Successful");
-                    }
-                    else {
-                          record.setIsSync("E");
-                        record.setSyncMessage(res.getErrorMsgs().get(0).getCode());
-                    }
+                        HttpHeaders headers = new HttpHeaders();
+                        MimeType mimeType = MimeTypeUtils.parseMimeType("application/json");
+                        MediaType mediaType = new MediaType(mimeType.getType(), mimeType.getSubtype(), Charset.forName("UTF-8"));
+                        headers.setContentType(mediaType);
+                        if (OpType.UPDATE.equals(opType)) {
+                            HttpEntity<Map> entity = new HttpEntity<Map>(Employee.createEmpMap(employee), headers);
+                            result = template.exchange(request.getRequestUrl(MasterDataType.EMPLOYEE, opType), HttpMethod.PUT, entity, String.class);
 
+                        }
+                        if (OpType.DELETE.equals(opType)) {
+                            result = template.exchange(request.getRequestUrl(MasterDataType.EMPLOYEE, opType), HttpMethod.PUT, null, String.class);
+                        }
                     }
-                    taskService.updateTask(record);
-                    logger.info(">>>>>>>>>>>>>>同步员工主数据结果：" + result.toString());
-                }
-                else {
-                    taskService.updateTask(taskRecords, false, result.getBody().toString());
-                    logger.info(">>>>>>>>>>>>>>同步员工主数据异常：" + result.toString());
+                    if (result.hasBody()) {
+                        OrgResponse res = (OrgResponse) mapper.readValue(result.getBody(), OrgResponse.class);
+                        taskService.updateTask(taskRecords, res.getSuccess(), res.getSuccess()?"Successful":"Failed");
+                        logger.info(">>>>>>>>>>>>>>同步员工主数据结果：" + result.toString());
+                    } else {
+                        taskService.updateTask(taskRecords, false, result.getBody().toString());
+                        logger.error(">>>>>>>>>>>>>>同步员工主数据异常：" + result.getStatusCode() + result.getBody());
+                    }
+                }catch (Exception e) {
+                    logger.error(">>>>>>>>>>>>>>同步员工主数据" + employee.getCode() + "异常：", e);
                 }
             }
-
-//            for (Employee employee:employees) {
-//                try{
-//                    // get department id by department code
-//                    ResponseEntity<String> resDept = template.getForEntity(request.getOrgUrl(MasterDataType.DEPARTMENT, employee.getDepartCode()), String.class);
-//                    if(resDept.hasBody()){
-//                        List<DefaultValue> defaultValues = mapper.readValue(resDept.getBody(), new TypeReference<List<DefaultValue>>() {});
-//                        if(defaultValues.size() == 0) {
-//                            logger.error(">>>>>>>>>>>>>>同步员工主数据失败：根据员工部门code在OA未找到匹配的部门");
-//                            return;
-//                        }
-//                        employee.setOrgDepartmentId(defaultValues.get(0).getId());
-//                    }
-//                    // call oa service push employee
-//                    Map emMap = Employee.createEmpMap(employee);
-//                    String jsonResult = mapper.writerWithDefaultPrettyPrinter()
-//                            .writeValueAsString(emMap);
-//                    ResponseEntity<String> result = template.postForEntity(request.getRequestUrl(MasterDataType.EMPLOYEE, opType), emMap, String.class);
-//                    if (result.hasBody()) {
-//                        OrgResponse res = (OrgResponse) mapper.readValue(result.getBody(), OrgResponse.class);
-//                        taskService.updateTask(taskRecords, res.getSuccess(), res.getSuccess()?"Successful":"Failed");
-//                        logger.info(">>>>>>>>>>>>>>同步员工主数据结果：" + result.toString());
-//                    } else {
-//                        taskService.updateTask(taskRecords, false, result.getBody().toString());
-//                        logger.error(">>>>>>>>>>>>>>同步员工主数据异常：" + result.getStatusCode() + result.getBody());
-//                    }
-//                }catch (Exception e) {
-//                    logger.error(">>>>>>>>>>>>>>同步员工主数据" + employee.getCode() + "异常：", e);
-//                }
-//            }
         } catch (Exception e) {
             logger.error(">>>>>>>>>>>>>>同步员工主数据异常：", e);
         }
